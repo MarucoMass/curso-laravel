49024c45dbc70dc2099089c987f3342f08581d40
MarucoMass <mariomass23@gmail.com>
2024-02-21 16:44:05 -0300

    agregué rutas nombradas

--- blade/resources/views/layouts/_partials/menu.blade.php
+++ blade/resources/views/layouts/_partials/menu.blade.php
@@ -1,6 +1,6 @@
 <ul>
-    <li><a href="">Home</a></li>
-    <li><a href="">About</a></li>
-    <li><a href="">Services</a></li>
-    <li><a href="">Contact</a></li>
+    <li><a href="{{route('index')}}">Home</a></li>
+    <li><a href="{{ route('about')}}">About</a></li>
+    <li><a href="{{ route('services') }}">Services</a></li>
+    <li><a href="{{ route('contact') }}">Contact</a></li>
 </ul>
