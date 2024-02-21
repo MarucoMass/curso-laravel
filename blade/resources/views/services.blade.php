e36292aa7ba907268dbc8b52d6537d737f75cc39
MarucoMass <mariomass23@gmail.com>
2024-02-21 16:58:41 -0300

    cree componente blade

--- blade/resources/views/services.blade.php
+++ blade/resources/views/services.blade.php
@@ -4,4 +4,12 @@
 
 @section('content')
     <h1>Services</h1>
+    @component('_components.card')
+        @slot('title', 'Service 1')
+        @slot('content', 'Lorem impsum dolor ar atste.')
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Service 2')
+        @slot('content', 'Lorem impsum.')
+    @endcomponent
 @endsection
