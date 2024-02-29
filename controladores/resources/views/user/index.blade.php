67c73390fd639ae17618e97df2ddc6218a2eeb48
MarucoMass <mariomass23@gmail.com>
2024-02-29 18:06:56 -0300

    asocie el controlador a una vista

---
+++ controladores/resources/views/user/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body{
+            min-height: 100dvh;
+            display: grid;
+            place-items: center;
+            background: #c1c1c1;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola capo</h1>
+</body>
+</html>
